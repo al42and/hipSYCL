4ed0bbc05cc4dfa13cc6258ba8eb52f9341af64b
Andrey Alekseenko <al42and@gmail.com>
2025-02-16 01:46:08 +0100

    Use Tina instead of Boost.Fiber

--- include/hipSYCL/glue/generic/host/collective_execution_engine.hpp
+++ include/hipSYCL/glue/generic/host/collective_execution_engine.hpp
@@ -14,20 +14,18 @@
 #include "hipSYCL/sycl/libkernel/backend.hpp"
 
 /**
- * Due to an issue with Boost.Intrusive (used by Boost.Fiber), on Windows,
- * which is triggered in device pass of Clang CUDA, we may only use this in host pass.
- * This should not be a problem, as this implementation is anyways just required during host pass.
+ * Allow disabling fibers; and don't try using them in device pass.
  */
-#if !defined(HIPSYCL_NO_FIBERS) && !defined(SYCL_DEVICE_ONLY)
-#define HIPSYCL_HAS_FIBERS
+#if !defined(ACPP_NO_FIBERS) && !defined(SYCL_DEVICE_ONLY)
+#define ACPP_HAS_FIBERS
 #endif
 
-#ifdef HIPSYCL_HAS_FIBERS
+#ifdef ACPP_HAS_FIBERS
 
 #include <functional>
+#include <vector>
 
-#include <boost/fiber/fiber.hpp>
-#include <boost/fiber/barrier.hpp>
+#include "tina.h"
 
 #include "hipSYCL/sycl/libkernel/range.hpp"
 #include "hipSYCL/sycl/libkernel/id.hpp"
@@ -45,18 +43,28 @@ enum class group_execution_iteration {
   sequential
 };
 
+namespace yield_kind {
+// Some odd value not easily confused with pointers
+static void* spawn = reinterpret_cast<void*>(0xff07);
+static void* barrier = reinterpret_cast<void*>(0xff09);
+static void* next_item = reinterpret_cast<void*>(0xff11);
+}
+static constexpr size_t fiber_stack_size = 256*1024;
+
+
 template<int Dim>
 class collective_execution_engine {
 public:
   collective_execution_engine(
       sycl::range<Dim> num_groups, sycl::range<Dim> local_size,
       sycl::id<Dim> offset,
-      const static_range_decomposition<Dim> &group_range_decomposition,
+      const static_range_decomposition<Dim>& group_range_decomposition,
       int my_group_region)
       : _num_groups{num_groups}, _local_size{local_size}, _offset{offset},
-        _group_barrier{local_size.size()}, _fibers_spawned{false},
-        _fibers(local_size.size()), _groups{group_range_decomposition},
-        _my_group_region{my_group_region} {}
+        _fibers_spawned{false}, _fibers(local_size.size(), nullptr),
+        _groups{group_range_decomposition}, _my_group_region{my_group_region},
+        _current_coro{nullptr} {}
+
 
   template <class WorkItemFunction>
   void run_kernel(WorkItemFunction f) {
@@ -64,76 +72,148 @@ public:
     _fibers_spawned = false;
     _master_group_position = 0;
 
-    // Try sequential processing (using only one fiber) - if
-    // other fibers need to be spawned, only process first work item
-    // as other work items will be processed by other fibers
-    _fibers[0] = boost::fibers::fiber([this]() {
-      _groups.for_each_local_element(
-          _my_group_region, [this](sycl::id<Dim> group_id) {
-            if (!_fibers_spawned) {
-              iterate_range(_local_size, [&](sycl::id<Dim> local_id) {
-                if (!_fibers_spawned)
-                  execute_work_item(local_id, group_id);
-              });
-            } else {
-              barrier();
-              // Only execute work item 0 from now on
-              execute_work_item(sycl::id<Dim>{}, group_id);
-            }
-            ++_master_group_position;
-          });
-    });
+    // Create master fiber
+    _fibers[0] = tina_init(nullptr, fiber_stack_size, &master_coro, this);
 
-    if (_fibers.size() > 0) {
-      _fibers[0].join();
-      if(_fibers_spawned){
-        for (std::size_t i = 1; i < _fibers.size(); ++i) {
-          _fibers[i].join();
-        }
-      }
+    bool all_done = false;
+
+    // Launch master fiber
+    void* result = resume(_fibers[0]);
+    if (_fibers[0]->completed) {
+      all_done = true;
+    } else {
+      // Encountered a barrier, call for help
+      assert(result == yield_kind::spawn);
+      spawn_fibers();
     }
 
+    while (!all_done) {
+      all_done = true;
+      // Process all coroutines
+      // The only way for the coroutine to yield is either a barrier or quitting.
+      // We can't ensure whether all the work-items hit the *same* barrier, and 
+      // finished work-iterms don't have to wait on barriers, 
+      // so we just cycle through all active coroutines and hope for the best.
+      void* master_yield_kind = nullptr;
+      for (auto& coro : _fibers) {
+        assert(coro);
+        if (!coro->completed) {
+          void* result = resume(coro);
+          if (!coro->completed) {
+            assert(result == yield_kind::barrier || result == yield_kind::next_item);
+            if (master_yield_kind == nullptr) master_yield_kind = result;
+            assert(result == master_yield_kind && "Fibers yielded for different reasons on the same pass");
+            all_done = false;
+          }
+        }
+      }
+    };
+
+    // Cleanup
+    for(auto& coro : _fibers) {
+      if(coro) {
+        free(coro->buffer); // This frees coro itself too
+        coro = nullptr;
+      }
+    }
+  }
+
+  void* resume(tina* coro) {
+    _current_coro = coro;  // Track current coroutine for barriers
+    void* result = tina_resume(coro, nullptr);
+    _current_coro = nullptr;
+    return result;
   }
 
   void barrier() {
-    if(!_fibers_spawned){
-      // We are still in sequential processing mode,
-      // need to spawn the other fibers
-      spawn_fibers();
-      // Perform additional barrier on master fiber
-      // to participate in the other fibers initial barrier
-      // when entering the first group
-      barrier();
+    assert(_current_coro != nullptr && "Barrier called outside coroutine context");
+    if (!_fibers_spawned) {
+      // Technically, we can only yield once here, but it's more explicit this way
+      tina_yield(_current_coro, yield_kind::spawn);
+      tina_yield(_current_coro, yield_kind::next_item);
     }
-    
-    _group_barrier.wait();
+    tina_yield(_current_coro, yield_kind::barrier);
   }
 
 private:
-  // Spawn remaining fibers
+  sycl::range<Dim> _num_groups;
+  sycl::range<Dim> _local_size;
+  sycl::id<Dim> _offset;
+  bool _fibers_spawned;
+  std::vector<tina*> _fibers;
+  std::function<void(sycl::id<Dim>, sycl::id<Dim>)> _kernel;
+  size_t _master_group_position;
+  const static_range_decomposition<Dim>& _groups;
+  int _my_group_region;
+
+  tina* _current_coro;
+
+  static void* master_coro(tina* coro, void* arg) {
+    auto* engine = static_cast<collective_execution_engine*>(coro->user_data);
+    engine->master_coro_body(coro);
+    return nullptr;
+  }
+
+  void master_coro_body(tina* coro) {
+    _groups.for_each_local_element(
+      _my_group_region, [this, coro](sycl::id<Dim> group_id) {
+        if(!_fibers_spawned) {
+          iterate_range(_local_size, [&](sycl::id<Dim> local_id) {
+            if(!_fibers_spawned) {
+              execute_work_item(local_id, group_id);
+            }
+          });
+        } else {
+          assert(coro == _current_coro);
+          tina_yield(coro, yield_kind::next_item);
+          assert(coro == _current_coro);
+          execute_work_item(sycl::id<Dim>{}, group_id);
+        }
+        ++_master_group_position;
+      });
+  }
+
+  struct CoroutineData {
+    collective_execution_engine* engine;
+    sycl::id<Dim> local_id;
+    size_t master_offset;
+  };
+
+  static void* worker_coro(tina* coro, void* arg) {
+    auto* data = static_cast<CoroutineData*>(coro->user_data);
+    auto* engine = data->engine;
+    const auto local_id = data->local_id;
+    const auto master_offset = data->master_offset;
+    delete data;
+
+    engine->worker_coro_body(coro, local_id, master_offset);
+    return nullptr;
+  }
+
+  void worker_coro_body(tina* coro, sycl::id<Dim> local_id, size_t master_offset) {
+    size_t current_group = 0;
+    _groups.for_each_local_element(
+      _my_group_region, [&](sycl::id<Dim> group_id) {
+        if(current_group >= master_offset) {
+          assert(coro == _current_coro);
+          tina_yield(coro, yield_kind::next_item);
+          assert(coro == _current_coro);
+          execute_work_item(local_id, group_id);
+        }
+        current_group++;
+      });
+  }
+
   void spawn_fibers() {
-
-    std::size_t n = 0;
+    size_t n = 0;
     iterate_range(_local_size, [&](sycl::id<Dim> local_id) {
-      // First work item will be processed by master fiber
       if (n != 0) {
-        
-        std::size_t master_offset = _master_group_position;
-        _fibers[n] = boost::fibers::fiber([local_id, this, master_offset]() {
-          std::size_t current_group = 0;
-          _groups.for_each_local_element(
-              _my_group_region, [&, this](sycl::id<Dim> group_id) {
-                if (current_group >= master_offset) {
-                  barrier();
-                  execute_work_item(local_id, group_id);
-                }
-                ++current_group;
-              });
-        });
+        // TODO: Use PMR for allocation of CoroutineData and tina stack
+        auto* data = new CoroutineData{this, local_id, _master_group_position};
+        _fibers[n] = tina_init(nullptr, fiber_stack_size, &worker_coro, data);
       }
-      ++n;
+      n++;
     });
-
     _fibers_spawned = true;
   }
 
@@ -141,16 +221,6 @@ private:
     _kernel(local_id, group_id);
   }
 
-  sycl::range<Dim> _num_groups;
-  sycl::range<Dim> _local_size;
-  sycl::id<Dim> _offset;
-  boost::fibers::barrier _group_barrier;
-  bool _fibers_spawned;
-  std::vector<boost::fibers::fiber> _fibers;
-  std::function<void(sycl::id<Dim>, sycl::id<Dim>)> _kernel;
-  std::size_t _master_group_position;
-  const static_range_decomposition<Dim> &_groups;
-  int _my_group_region;
 };
 
 }
---
+++ src/runtime/tina_impl.cpp
@@ -0,0 +1,14 @@
+/*
+ * This file is part of AdaptiveCpp, an implementation of SYCL and C++ standard
+ * parallelism for CPUs and GPUs.
+ *
+ * Copyright The AdaptiveCpp Contributors
+ *
+ * AdaptiveCpp is released under the BSD 2-Clause "Simplified" License.
+ * See file LICENSE in the project root for full license details.
+ */
+// SPDX-License-Identifier: BSD-2-Clause
+
+#define TINA_IMPLEMENTATION
+#include "hipSYCL/glue/generic/host/tina.h"
+
